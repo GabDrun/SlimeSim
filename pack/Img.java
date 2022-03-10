edf63b3e3c345863bbc6c7ff3ff93012aae4e916
GabDrun <gabrielius.drungilas@gmail.com>
2022-03-10 21:37:59 +0200

    Further cleanup, and objectifying classes

--- pack/Img.java
+++ pack/Img.java
@@ -6,99 +6,81 @@ import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
-import java.util.concurrent.TimeUnit;
 
 public class Img {
     private JFrame frame;
     private JLabel label;
-    private int width;
-    private int height;
-    private int CHECK_REGION_SIZE;
-    Graphics2D g2D;
-    BufferedImage myImage, weightMap;
-    Kernel blur3x3 = new Kernel(3, 3, new float[]
-            { 0.04f, 0.08f, 0.04f,
-                    0.08f, 0.52f, 0.08f,
-                    0.04f, 0.08f, 0.04f });
-    Kernel boxBlur3x3 = new Kernel(3, 3, new float[]
+    private BufferedImageOp opBlur;
+
+    private Kernel blur3x3 = new Kernel(3, 3, new float[]
             { 0.111f, 0.111f, 0.111f,
                     0.111f, 0.111f, 0.111f,
                     0.111f, 0.111f, 0.111f });
-    Kernel blurFade3x3 = new Kernel(3, 3, new float[]
-            { 0.04f, 0.08f, 0.04f,
-                    0.08f, 0.501f, 0.08f,
-                    0.04f, 0.08f, 0.04f });
-    Kernel gausianFade3x3 = new Kernel(3, 3, new float[]
-            { 0.075f, 0.124f, 0.075f,
-                    0.124f, 0.204f, 0.124f,
-                    0.075f, 0.124f, 0.075f });
-    Kernel blurSlowFade3x3 = new Kernel(3, 3, new float[]
-            { 0.0083333f, 0.0166666f, 0.0083333f,
-                    0.0166666f, 0.89f, 0.0166666f,
-                    0.0083333f, 0.0166666f, 0.0083333f });
-    BufferedImageOp opBlur = new ConvolveOp(boxBlur3x3);
-    Kernel sum3x3 = new Kernel(3, 3, new float[]
-            { 1, 1, 1,
-                    1, 1, 1,
-                    1, 1, 1 });
-    BufferedImageOp sumFilter = new ConvolveOp(sum3x3);
+
+    private int width;
+    private int height;
+    private BufferedImage myImage;
 
 
     Img(int width, int height){
+        init(width, height, blur3x3);
+    }
+
+    Img(int width, int height, Kernel k){
+        init(width, height, k);
+    }
+
+    private void init(int width, int height, Kernel k){
         setWidth(width);
         setHeight(height);
-        myImage = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
-        //weightMap = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+        setMyImage(new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB));
+        setOpBlur(k);
     }
 
     public void brighten(int x, int y, int color){
-        myImage.setRGB(x, y, color);
+        getMyImage().setRGB(x, y, color);
     }
 
     private void blur(){
-        myImage = opBlur.filter(myImage,null);
+        setMyImage(opBlur.filter(getMyImage(),null));
     }
 
-    private static int darkerColor(int color){
-        float ratio = 0.9f;
-        int a = (color >> 24) & 0xFF;
-        int r = (int) (((color >> 16) & 0xFF) * ratio);
-        int g = (int) (((color >> 8) & 0xFF) * ratio);
-        int b = (int) ((color & 0xFF) * ratio);
-
-        return (a << 24) | (r << 16) | (g << 8) | b;
-    }
-
-    private void fade(){
-        for(int cx = 0; cx < myImage.getWidth();cx++){
-            for(int cy = 0; cy < myImage.getHeight();cy++){
-                myImage.setRGB(cx,cy,darkerColor(myImage.getRGB(cx,cy)));
-            }
-        }
-    }
-
-    public int checkRegionBrightness(float x, float y, float angle){
-        x += 20*Math.cos(angle);
-        y += 20*Math.sin(angle);
+    public int checkRegionBrightness(float x, float y, float angle, int visionRange){
+        x += visionRange * Math.cos(angle);
+        y += visionRange * Math.sin(angle);
 
         int val = 0;
-        for(int i = -1; i < 1;i++){
+        for(int i = -1; i < 1; i++){
             for(int j = -1; j < 1; j++){
-                if(x + i >= width || y + j >= height || x + i <= 0 || y + j <= 0){
+                if(x + i >= getWidth() || y + j >= getHeight() || x + i <= 0 || y + j <= 0){
                     continue;
                 }
-                val += myImage.getRGB((int)x + i,(int)y + j);
+                val += getMyImage().getRGB((int)x + i,(int)y + j);
             }
         }
         return val;
-//        if(x >= width || y >= height || x <= 0 || y <= 0)
-//            return 0;
-//        return weightMap.getRGB((int)x,(int)y);
+    }
 
-//        if(x >= width || y >= height || x <= 0 || y <= 0)
-//            return 0;
-//        return myImage.getRGB((int)x,(int)y);
+    private void display(){
+        if(frame == null){
+            frame = (new JFrame());
+            frame.setTitle("My Image");
+            frame.setSize(getMyImage().getWidth(), getMyImage().getHeight());
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            label = (new JLabel());
+            label.setIcon(new ImageIcon(getMyImage()));
+            frame.getContentPane().add(label, BorderLayout.CENTER);
+            frame.setLocationRelativeTo(null);
+            frame.pack();
+            frame.setVisible(true);
+        }else {
+            label.setIcon(new ImageIcon(getMyImage()));
+        }
+    }
 
+    public void updatePicture(){
+        blur();
+        display();
     }
 
     public int getWidth() {
@@ -116,31 +98,20 @@ public class Img {
     private void setHeight(int height) {
         this.height = height;
     }
-    private int counter;
-    public void updatePicture(){
-        blur();   // really slow
-        // fade // now blur fades
-        display();
-       // ++counter;
+
+    public BufferedImage getMyImage() {
+        return myImage;
     }
 
-    public void display(){
-        if(frame==null){
-            frame=new JFrame();
-            frame.setTitle("Slime Simulation");
-            frame.setSize(myImage.getWidth(), myImage.getHeight());
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            label=new JLabel();
-            label.setIcon(new ImageIcon(myImage));
-            frame.getContentPane().add(label,BorderLayout.CENTER);
-            frame.setLocationRelativeTo(null);
-            frame.pack();
-            frame.setVisible(true);
+    private void setMyImage(BufferedImage myImage) {
+        this.myImage = myImage;
+    }
 
-//            for (int i = 0; i < 1000; i++)frame.setVisible(true);    // wait
+    public Kernel getBlur3x3() {
+        return blur3x3;
+    }
 
-        }else {
-            label.setIcon(new ImageIcon(myImage));
-        }
+    private void setOpBlur(Kernel k) {
+        this.opBlur = new ConvolveOp(k);
     }
 }
--- pack/Point.java
+++ pack/Point.java
@@ -5,110 +5,140 @@ import java.util.Random;
 
 public class Point {
     private static final Random rand = new Random();
-    private static final boolean reverse = true;
-    private static float speed = 1f;
-    private static float turnAngle = 0.1f;
-    private static final float checkAngle = 0.1f;
-    private static final float randomTurnAngle = 0.01f;
+    private static Img map;
+
+    private static final int SPAWN_RADIUS = 1;
+    private static final float TURN_ANGLE = 0.1f;
+    private static final float CHECK_ANGLE = 0.1f;
+    private static final float RANDOM_TURN_ANGLE = 0.25f;
+    private static int VISION_RANGE = 20;
+
     private static int maxWidth;
     private static int maxHeight;
-    private static Img map;
-    //private Color white = new Color(255,255,255);
-    //private static final Color green = new Color(0,204,0);
-    //private static final Color reddish = new Color(245,147,115);
+
+    private static final Color white = new Color(255,255,255);
+    private static final Color green = new Color(0,204,0);
+    private static final Color reddish = new Color(245,147,115);
     private static final Color yellow = new Color(250,227,119);
     private static final int myColor = yellow.getRGB();
-    private static final int SPAWN_RADIUS = 1;
 
+    private float x;
+    private float y;
+    private float speed;
     private float angle;
-    private float x = 0;
-    private float y = 0;
-
 
 
     Point(Img map){
         Point.map = map;
-        maxWidth = map.getWidth()-1;
-        maxHeight = map.getHeight()-1;
-        this.x = rand.nextInt(0, maxWidth);
-        this.y = rand.nextInt(0, maxHeight);
-        this.x = rand.nextInt(maxWidth/2 -SPAWN_RADIUS, maxWidth/2 +SPAWN_RADIUS);
-        this.y = rand.nextInt(maxHeight/2 -SPAWN_RADIUS, maxHeight/2 +SPAWN_RADIUS);
-        this.angle = rand.nextFloat(0,6.28318f);
-        //this.angle = 0f;
-
+        setMaxHeight(map.getHeight()-1);
+        setMaxWidth(map.getWidth()-1);
+//        setX(rand.nextInt(0, maxWidth));
+//        setY(rand.nextInt(0, maxHeight));
+        setX(rand.nextInt(getMaxWidth()/2 -SPAWN_RADIUS, getMaxWidth()/2 +SPAWN_RADIUS));
+        setY(rand.nextInt(getMaxHeight()/2 -SPAWN_RADIUS, getMaxHeight()/2 +SPAWN_RADIUS));
+        setAngle(rand.nextFloat(0, 2 * (float)Math.PI));
+        setSpeed(1f + rand.nextFloat(-0.7f,2f));
     }
 
-    public float getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
-    }
-
-    public float getAngle() {
-        return angle;
-    }
-
-    public void setAngle(float angle) {
-        this.angle = angle;
-    }
-
-    private void sense(){
-        float leftAngle = angle+checkAngle;
-        float rightAngle = angle-checkAngle;
-        int left = map.checkRegionBrightness(x,y,leftAngle);//& 0xFF;
-        int straight = map.checkRegionBrightness(x,y,angle);//& 0xFF;
-        int right = map.checkRegionBrightness(x,y,rightAngle);//& 0xFF;
+    private void turnPoint(){
+        float leftAngle = getAngle() + CHECK_ANGLE;
+        float rightAngle = getAngle() - CHECK_ANGLE;
+        int left = map.checkRegionBrightness(getX(), getY(), leftAngle, VISION_RANGE);//& 0xFF;
+        int straight = map.checkRegionBrightness(getX(), getY(), getAngle(), VISION_RANGE);//& 0xFF;
+        int right = map.checkRegionBrightness(getX(), getY(), rightAngle, VISION_RANGE);//& 0xFF;
 
         if(left>straight && left>right)
-            angle += turnAngle;                 // flip signs for avoiding effect
+            setAngle(getAngle() + TURN_ANGLE + rand.nextFloat(-RANDOM_TURN_ANGLE, RANDOM_TURN_ANGLE));               // flip signs for avoiding effect
         else if(right>straight && right>left)
-            angle -= turnAngle;                 // flip signs for avoiding effect
+            setAngle(getAngle() - TURN_ANGLE + rand.nextFloat(-RANDOM_TURN_ANGLE, RANDOM_TURN_ANGLE));               // flip signs for avoiding effect
+        else setAngle(getAngle() + rand.nextFloat(-RANDOM_TURN_ANGLE, RANDOM_TURN_ANGLE));
+    }
 
-        angle += 25*rand.nextFloat(-randomTurnAngle,randomTurnAngle);
+    private void collisionCheck(){
+        if((int)getX() >= getMaxWidth()){
+            setX(2 * getMaxWidth() - getX());
+            setAngle((float) Math.PI - getAngle());
+        }else if((int)getX() <= 0){
+            setX(-getX());
+            setAngle((float) Math.PI - getAngle());
+        }
+
+        if((int)getY() >= getMaxHeight()){
+            setY(2 * getMaxHeight() - getY());
+            setAngle(-getAngle());
+        }else if((int)getY() <= 0){
+            setY(-getY());
+            setAngle(-getAngle());
+        }
     }
 
     private void move(){
-        // TODO sense();
-        sense();
-        x += speed * Math.cos(angle);
-        y += speed * Math.sin(angle);
-        if((int)x >= maxWidth){
-            x = 2*maxWidth-x;
-            angle = (float) 3.14159-angle;
-        }else if((int)x <= 0){
-            x = -x;
-            angle = (float) 3.14159-angle;
-        }
-        if((int)y >= maxHeight){
-            y = 2*maxHeight-y;
-            angle = -angle;
-        }else if((int)y <= 0){
-            y = -y;
-            angle = -angle;
-        }
+        setX(getX() + getSpeed() * (float)Math.cos(getAngle()));
+        setY(getY() + getSpeed() * (float)Math.sin(getAngle()));
     }
 
     public void update(){
+        turnPoint();
         move();
-        map.brighten((int)x, (int)y, myColor);
+        collisionCheck();
+        map.brighten((int)getX(), (int)getY(), myColor);
+    }
+
+    private float getX() {
+        return x;
+    }
+
+    private void setX(float x) {
+        this.x = x;
+    }
+
+    private float getY() {
+        return y;
+    }
+
+    private void setY(float y) {
+        this.y = y;
+    }
+
+    private float getSpeed() {
+        return speed;
+    }
+
+    private void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    private float getAngle() {
+        return angle;
+    }
+
+    private void setAngle(float angle) {
+        this.angle = angle;
+    }
+
+    private static int getMaxWidth() {
+        return maxWidth;
+    }
+
+    private static void setMaxWidth(int maxWidth) {
+        Point.maxWidth = maxWidth;
+    }
+
+    private static int getMaxHeight() {
+        return maxHeight;
+    }
+
+    private static void setMaxHeight(int maxHeight) {
+        Point.maxHeight = maxHeight;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", speed=" + speed +
+                ", angle=" + angle +
+                '}';
     }
 }
